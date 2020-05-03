9640f6ac115a1568351d535f4347a92466b14c90
PAPA <PAPA@iMac-de-Miguel.local>
2020-05-03 20:15:21 +0200

    Removed typo and double dots at moon fase phrase

--- tweet_moon_phase.py
+++ tweet_moon_phase.py
@@ -21,28 +21,28 @@ def lunar_phase_emoji(lunation_number):
   #if lunation_number == 0:
   if lunation_number >= 0.98 or lunation_number < 0.02:
     moon_emoji = emoji.emojize(':new_moon_face:')
-    fase_cat = "lluna nova."
+    fase_cat = "lluna nova"
   elif lunation_number >= 0.03 and lunation_number<0.20:
     moon_emoji = emoji.emojize(':waxing_crescent_moon:')
-    fase_cat = "lluna creixent,."
+    fase_cat = "lluna creixent"
   elif lunation_number >= 0.20  and lunation_number < 0.30:
     moon_emoji = emoji.emojize(':first_quarter_moon:')
-    fase_cat = "quart creixent."
+    fase_cat = "quart creixent"
   elif lunation_number >= 0.30 and lunation_number<0.4:
     moon_emoji = emoji.emojize(':waxing_gibbous_moon:')
-    fase_cat = "lluna gibosa creixent."
+    fase_cat = "lluna gibosa creixent"
   elif lunation_number >= 0.45 and lunation_number < 0.55:
     moon_emoji = emoji.emojize(':full_moon:')
-    fase_cat = "lluna plena."
+    fase_cat = "lluna plena"
   elif lunation_number >= 0.6 and lunation_number<0.7:
     moon_emoji = emoji.emojize(':waning_gibbous_moon:')
-    fase_cat = "lluna gibosa minvant."
+    fase_cat = "lluna gibosa minvant"
   elif lunation_number >= 0.75 and lunation_number < 0.80:
     moon_emoji = emoji.emojize(':last_quarter_moon:') 
-    fase_cat = "quart minvant."
+    fase_cat = "quart minvant"
   elif lunation_number >=  0.80 and lunation_number < 0.90:
     moon_emoji = emoji.emojize(':waning_crescent_moon:')
-    fase_cat = "lluna minvant."
+    fase_cat = "lluna minvant"
   return (moon_emoji, fase_cat)
 
 
@@ -58,7 +58,7 @@ moon_emoji,fase_cat = lunar_phase_emoji(lunation_number)
 print (moon_emoji)
 
 
-fase_lunar = f"Bona nit. Fase lunar d'avui: {moon_emoji} {fase_cat}. Nombre llunàtic:{lunation_number}" 
+fase_lunar = f"Bona nit. Fase lunar d'avui: {moon_emoji} {fase_cat}. Nombre llunàtic: {lunation_number}" 
 
 def get_api(cfg):
   auth = tweepy.OAuthHandler(cfg['consumer_key'], cfg['consumer_secret'])
